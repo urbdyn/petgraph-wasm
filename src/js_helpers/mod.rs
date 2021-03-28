d6ec5588468b4bc6fadf6b539c1586234e5f32e6
Ferris Ellis <3579192+code-ape@users.noreply.github.com>
2021-03-28 23:44:20 +0000

    Moved unit tests to src/

---
+++ src/js_helpers/mod.rs
@@ -0,0 +1,5 @@
+//! This file contains "helpers" for the petgraph-wasm library which are
+//! needed in addition to the raw petgraph features so that the library is usable
+//! from Javascript.
+
+pub mod test;
